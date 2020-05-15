729c612582d699649b1610646562d7b409cd3e58
Paweł <czubak.official@gmail.com>
2020-05-15 03:17:10 +0200

    initial

---
+++ APBD/Models/Enrollment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3.Models
+{
+    public class Enrollment
+    {
+        public int Semester { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public Study Study { get; set; }
+
+        public Enrollment()
+        {
+
+        }
+
+        public static Enrollment newEnrollment(int semester, Study study, DateTime startDate)
+        {
+            Enrollment enrollment = new Enrollment();
+
+            enrollment.Semester = semester;
+            enrollment.StartDate = startDate;
+            enrollment.Study = study;
+
+            return enrollment;
+        }
+
+
+    }
+}
---
+++ APBD/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3.Models
+{
+    public class Student
+    {
+        //I had to use public bacuause of JSON serialization 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IndexNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Enrollment Enrollment { get; set; }
+
+        public Student(){
+        }
+
+        public static Student newStudent(string indexNumber, string firstName, string lastName, DateTime birthDate, Enrollment enrollment)
+        {
+            Student student = new Student();
+
+            student.IndexNumber = indexNumber;
+            student.FirstName = firstName;
+            student.LastName = lastName;
+            student.BirthDate = birthDate;
+            student.Enrollment = enrollment;
+
+            return student;
+        }
+      
+    }
+}
