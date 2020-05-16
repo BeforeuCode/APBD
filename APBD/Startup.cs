8cd67f64f8f8625107c43e6f091c2c8593e37bc1
Paweł <czubak.official@gmail.com>
2020-05-16 16:32:05 +0200

    Added middlewares

--- APBD/Startup.cs
+++ APBD/Startup.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using cw3.DAL;
 using cw3.DAL.Services;
+using cw3.Middlewares;
+using cw3.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -32,7 +35,7 @@ namespace cw3
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IStudentsDbService studentsDb)
         {
             if (env.IsDevelopment())
             {
@@ -44,6 +47,30 @@ namespace cw3
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseMiddleware<LoggingMiddleware>();
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("No index provided");
+                    return;
+                }
+
+                string index = context.Request.Headers["Index"].ToString();
+
+                Student student = studentsDb.GetStudentByIndex(index);
+                if(student != null)
+                {
+                    await next();
+                } else
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Student with that id doesnt exist");
+                    return;
+                }   
+             });
 
             app.UseEndpoints(endpoints =>
             {
