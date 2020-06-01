f744eeaa941dc1990f0a89d258380829a3a1f527
Paweł <czubak.official@gmail.com>
2020-06-01 19:23:51 +0200

    fix

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(50) NOT NULL, 
+    [Salt] VARCHAR(50) NOT NULL, 
+    [HashedPassword] VARCHAR(50) NOT NULL, 
+    [RefreshToken] VARCHAR(50) NULL, 
+    [Role] VARCHAR(50) NULL
+)
